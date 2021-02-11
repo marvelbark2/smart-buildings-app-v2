246d95c46e62905879f4a933c2b3e19904e1ac62
Youness Masaoudi <marvelbark2@gmail.com>
2021-02-11 19:54:03 +0100

    Adding roles and testmode to client with main method

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/Client.java
@@ -0,0 +1,37 @@
+package edu.episen.si.ing1.pds.client;
+
+import edu.episen.si.ing1.pds.client.roles.RoleType;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private final static Logger logger = LoggerFactory.getLogger(Client.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        options.addOption(testMode);
+
+        final Option userRole = Option.builder().longOpt("userRole").hasArg().build();
+        options.addOption(userRole);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        boolean itestMode = false;
+        if (commandLine.hasOption("testMode"))
+            itestMode = true;
+
+        RoleType roleType = null;
+        if (commandLine.hasOption("userRole")){
+            int id = Integer.parseInt(commandLine.getOptionValue("userRole"));
+            roleType = RoleType.Instance.findRole(id);
+            System.out.println(roleType);
+        }
+
+
+        logger.info("Client is running (testMode = {}, under a {}", itestMode , roleType);
+
+    }
+}
---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/roles/RoleType.java
@@ -0,0 +1,25 @@
+package edu.episen.si.ing1.pds.client.roles;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum RoleType {
+    Instance(0),ADMIN(1),EMPLOYEE(2),COMPANY(3),SG(4);
+    private int degree;
+
+    private RoleType(int degree){
+        this.degree = degree;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+    public RoleType findRole(int id) {
+        return (id != 0)
+                ? Arrays.stream(RoleType.values())
+                        .filter(role -> role.getDegree() == id)
+                        .findFirst()
+                        .get()
+                : null;
+    }
+}
