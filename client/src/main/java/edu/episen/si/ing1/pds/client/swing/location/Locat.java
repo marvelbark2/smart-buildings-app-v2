df2bfb6e6a72f24ef4628e1c3f7bcc150859b50d
ludoweh <ludovicwehrung@gmail.com>
2021-04-27 23:15:12 +0200

    updating page for location

--- client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Locat.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Locat.java
@@ -16,11 +16,26 @@ public class Locat implements Way {
     JTextField tailleMin = new JTextField(10);
     JTextField prixMin = new JTextField(10);
     JTextField prixMax = new JTextField(10);
+    JCheckBox screen =new JCheckBox("ecran");
+    JCheckBox captor =new JCheckBox("capteur");
+    JCheckBox prise =new JCheckBox("prise");
+    
+    int answer =5;
+    JLabel[] proposition = new JLabel[5];
+    JButton[] reserv = new JButton[5];
+    
+    
     JButton valid =new JButton("valider");
 
 	public void begin(LocationMenu men) {
 		JPanel panrech = men.getApp().getContext();
 		 JPanel p = new JPanel( );
+		 
+		 JLabel Nbdesk = new JLabel("nombre de bureau fermé",JLabel.LEFT);
+		 Nbdesk.setLabelFor(nbBureau);
+		 
+		 JLabel Nbopenspace = new JLabel("nombre d'openspace",JLabel.LEFT);
+		 Nbopenspace.setLabelFor(nbOpenspace);
 		
 		 JLabel Tmin = new JLabel("taille minimale", JLabel.LEFT);
 		    Tmin.setLabelFor(tailleMax);
@@ -36,13 +51,22 @@ public class Locat implements Way {
 		    
 		   
 		    JButton retButton = new JButton("retour");
-		    p.setLayout(new GridLayout(7, 1));
+		    p.setLayout(new GridLayout(12, 1));
 		
 		    
 		    valid.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					
 						p.removeAll();
+						
+						for (int i = 0; i<5;i++) {
+							proposition[i]=new JLabel("pos");// le texte change en fonction du bâtiment
+					    	  proposition[i].setText("cette offre contient x bureau fermé et y openspace pour une taille de z metre carré et un prix de x euros ");
+					    	  p.add(proposition[i]);
+					    	  reserv[i]= new JButton("réserver"); 
+					          p.add(reserv[i]);
+						}
+						
 					     p.add(retButton) ;
 					     p.invalidate();
 						 p.validate();
@@ -61,6 +85,10 @@ public class Locat implements Way {
 					
 				    p.add(bureau_ferme);
 				    p.add(openspace);
+				    p.add(Nbdesk);
+				    p.add(nbBureau);
+				    p.add(Nbopenspace);
+				    p.add(nbOpenspace);
 				    p.add(Tmin);
 				    p.add(tailleMin);
 				    p.add(Tmax);
@@ -68,7 +96,10 @@ public class Locat implements Way {
 				    p.add(PersMax);
 				    p.add(prixMin);
 				    p.add(Pmax);
-				    p.add(prixMax);   
+				    p.add(prixMax);  
+				    p.add(screen);
+				    p.add(prise);
+				    p.add(captor);
 				    p.add(valid);
 					p.invalidate();
 					 p.validate();
@@ -82,6 +113,10 @@ public class Locat implements Way {
 		        
 		    p.add(bureau_ferme);
 		    p.add(openspace);
+		    p.add(Nbdesk);
+		    p.add(nbBureau);
+		    p.add(Nbopenspace);
+		    p.add(nbOpenspace);
 		    p.add(Tmin);
 		    p.add(tailleMin);
 		    p.add(Tmax);
@@ -89,7 +124,10 @@ public class Locat implements Way {
 		    p.add(PersMax);
 		    p.add(prixMin);
 		    p.add(Pmax);
-		    p.add(prixMax);   
+		    p.add(prixMax); 
+		    p.add(screen);
+		    p.add(prise);
+		    p.add(captor);
 		    p.add(valid);
 		    panrech.add(p);
 	}
