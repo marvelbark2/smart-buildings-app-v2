ce3046142a9309db3219100a76facd5a2355add9
Selim <selim@MacBook-Pro-de-bouhassatine.local>
2021-04-30 17:24:25 +0200

    Adding the start of a drag and drop system for mapping_window

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/workspace/mapping/MappingNetwork.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/workspace/mapping/MappingNetwork.java
@@ -1,11 +1,13 @@
 package edu.episen.si.ing1.pds.backend.server.workspace.mapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.episen.si.ing1.pds.backend.server.network.Request;
 import edu.episen.si.ing1.pds.backend.server.utils.Utils;
 
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MappingNetwork {
-    public MappingNetwork(Request request, Connection connection, PrintWriter writer) {
+    public MappingNetwork(Request request, Connection connection, PrintWriter writer) throws SQLException, JsonProcessingException {
         
     	String event = request.getEvent();
         ObjectMapper mapper = new ObjectMapper();
@@ -80,6 +82,53 @@ public class MappingNetwork {
             } catch (Exception e ){
                 e.printStackTrace();
             }
+        }
+        else if(event.equalsIgnoreCase("tree_list")){
+        	 String sql = "SELECT b.id_buildings, b.name, f.floor_number,workspace.id_workspace, workspace.workspace_type FROM workspace\n" +
+                     "join floors f on f.id_floor = workspace.floor_number\n" +
+                     "join buildings b on b.id_buildings = f.building_number";
+             PreparedStatement statement = connection.prepareStatement(sql);
+             List<Map> responseList = new ArrayList<>();
+             ResultSet rs = statement.executeQuery();
+             Map<Map, List> data = new HashMap();
+        
+             
+             while (rs.next()) {
+                 Map hm = new HashMap();
+                 hm.put("id_buildings", rs.getInt("id_buildings"));
+                 hm.put("name", rs.getString("name"));
+
+
+                 Map<Map, List> floorContainer = new HashMap<>();
+                 Map floor = new HashMap();
+                 floor.put("floor_number", rs.getInt("floor_number"));
+
+
+                 Map workspace = new HashMap();
+                 workspace.put("id_workspace", rs.getInt("id_workspace"));
+                 workspace.put("workspace_type", rs.getString("workspace_type"));
+
+
+                 if(!floorContainer.containsKey(floor)) {
+                     List<Map> workspaces = new ArrayList<>();
+                     workspaces.add(workspace);
+                     floorContainer.put(floor, workspaces);
+                 } else {
+                     floorContainer.get(floor).add(workspace);
+                 }
+                 if(!data.containsKey(hm)) {
+                     List<Map> floors = new ArrayList<>();
+                     floors.add(floorContainer);
+                     data.put(hm , floors);
+                 } else {
+                     data.get(hm).add(floorContainer);
+                 }
+                 responseList.add(data);
+             };
+             Map responseMsg = Utils.responseFactory(responseList, event);
+             String serializedMsgString = mapper.writeValueAsString(responseMsg);
+             writer.println(serializedMsgString);
+              
         }
     }
 }
--- client/src/main/java/edu/episen/si/ing1/pds/client/swing/mapping/selim/Mapping_Window.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/swing/mapping/selim/Mapping_Window.java
@@ -1,34 +1,47 @@
 package edu.episen.si.ing1.pds.client.swing.mapping.selim;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.SocketException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
+import javax.swing.TransferHandler;
 import javax.swing.border.LineBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import org.w3c.dom.css.Rect;
+
 import edu.episen.si.ing1.pds.client.network.Request;
 import edu.episen.si.ing1.pds.client.network.Response;
-import edu.episen.si.ing1.pds.client.network.SocketConfig;
+
 import edu.episen.si.ing1.pds.client.swing.global.Main;
 import edu.episen.si.ing1.pds.client.swing.global.Navigate;
 import edu.episen.si.ing1.pds.client.utils.Utils;
+
+
 
 public class Mapping_Window implements Navigate {
     Main global;
@@ -40,26 +53,7 @@ public class Mapping_Window implements Navigate {
 
 
     }
-    
-    /*private void getBatiment() {
-    	try {
-    		SocketConfig.Instance.setEnv(true);
-    		Request request=new Request();
-
-    		JPanel panel = new JPanel();
-    		request.setEvent("building_list");
-            Response response = Utils.sendRequest(request);
-            List<Map> data = (List<Map>) response.getMessage();
-            for ( Map e: data) {
-                JLabel label = new JLabel(e.get("abbreviation").toString());
-                label.setBorder(new LineBorder(Color.BLUE));
-                panel.add(label);
-            }
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-        
-    }*/
+   
 
     private void menuScroll(JTree arbre2) {
         JPanel menuPanel = global.getMenu();
@@ -81,7 +75,7 @@ public class Mapping_Window implements Navigate {
    private JTree buildTree() {
     	
 	   	try {
-			SocketConfig.Instance.setEnv(true);
+		
 			Request request=new Request();
 			request.setEvent("building_list");
 			Response response = Utils.sendRequest(request);
@@ -109,6 +103,38 @@ public class Mapping_Window implements Navigate {
 		// TODO: handle exception
 	}
 		return arbre;
+	   
+	/*   try {
+		SocketConfig.Instance.setEnv(true);
+		Request request = new Request();
+		request.setEvent("tree_list");
+		Response response = Utils.sendRequest(request);
+		Map<Map, List> dataMap = new HashMap();
+		
+		DefaultMutableTreeNode racine = new DefaultMutableTreeNode("racine");
+		
+		for (Map building:dataMap.keySet()) {
+			DefaultMutableTreeNode buildNode = new DefaultMutableTreeNode(building.get("name"));
+            for (Object floor: dataMap.get(building)) {
+                Map<Map, List> floorN = (Map) floor;
+                for (Map<Map, List> works: floorN.keySet()) {
+                    DefaultMutableTreeNode floorNode = new DefaultMutableTreeNode("Etage " + works.get("floor_number"));
+                    buildNode.add(floorNode);
+                    for (List<Map> dataN: floorN.values()) {
+                        for (Map workspace: dataN) {
+                            DefaultMutableTreeNode workNode = new DefaultMutableTreeNode(workspace.get("workspace_type"));
+                            floorNode.add(workNode);
+                        }
+                    }
+                }
+            }
+            racine.add(buildNode);
+		}
+		arbre = new JTree(racine);
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+	   return arbre;*/
  }
 
     private JToolBar createToolBar() {
@@ -127,33 +153,104 @@ public class Mapping_Window implements Navigate {
 
         return toolBar;
     }
+    
+    public class DrawMyRect extends JPanel {
+    	
+	    public void paint(Graphics g) {
+	    	g.drawRect(50, 35, 150, 150);
+	    	g.setColor(Color.red);
 
+	    }
+    }
     private JPanel carte() {
-
-        JPanel carte = new JPanel(new GridLayout(3,3,10,10));
-        carte.setBorder(new LineBorder(Color.black));
-
-        JTextField emplacement1 = new JTextField("Deposer un équipement");
-        emplacement1.setBorder(new LineBorder(Color.GREEN));
-        carte.add(emplacement1);
-
-        JTextField emplacement2 = new JTextField("Deposer un équipement");
-        emplacement2.setBorder(new LineBorder(Color.GREEN));
-        carte.add(emplacement2);
-
-        JTextField emplacement3 = new JTextField("Deposer un équipement");
-        emplacement3.setBorder(new LineBorder(Color.RED));
-        carte.add(emplacement3);
-
-        JTextField emplacement4 = new JTextField("Deposer un équipement");
-        emplacement4.setBorder(new LineBorder(Color.GREEN));
-        carte.add(emplacement4);
-
-        JTextField emplacement5 = new JTextField("Deposer un équipement");
-        emplacement5.setBorder(new LineBorder(Color.RED));
-        carte.add(emplacement5);
-
-
+    	
+    	JPanel carte = new JPanel(new GridBagLayout());
+    	carte.setBorder(new LineBorder(Color.black));
+    	
+    	 JButton btn1 = new JButton();
+    	 btn1.setTransferHandler(new TransferHandler("text"));
+		 JButton btn2 = new JButton();
+		 btn2.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn3 = new JButton();
+		 btn3.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn4 = new JButton();
+		 btn4.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn5 = new JButton("Btn 5");
+		 btn5.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn6 = new JButton("Btn 6");
+		 btn6.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn7 = new JButton("Btn 7");
+		 btn7.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn8 = new JButton("Btn 8");
+		 btn8.setTransferHandler(new TransferHandler("icon"));
+		 JButton btn9 = new JButton("Btn 9");
+		 btn9.setTransferHandler(new TransferHandler("icon"));
+		 
+		 GridBagConstraints gcon = new GridBagConstraints();
+		 gcon.weightx = 1;
+		 gcon.weighty = 1;
+		 
+		 gcon.fill = GridBagConstraints.BOTH;
+		 
+		 // Prepare Contraints for Btn 1 
+		 gcon.gridx = 0;
+		 gcon.gridy = 0;
+		 gcon.gridwidth = 2;
+		 gcon.gridheight = 1;
+		 
+		 // Add Control
+		 carte.add(btn1, gcon);
+		 
+		 
+		 // Prepare Contraints for Btn2
+		 gcon.gridx = 2;
+		 gcon.gridy = 0;   // pas obliger
+		 gcon.gridwidth = 2;  // pas obliger
+		 gcon.gridheight = 1;  // pas obliger 
+		 
+		 // Add Control 
+		 
+		 carte.add(btn2, gcon);
+		 
+		 
+		 // Prepare Contraints for Btn3 
+		 gcon.gridy = 1;
+		 gcon.gridwidth = 1;
+		 gcon.gridx = 0;
+		 
+		 // Add Control
+		 carte.add(btn3,gcon);
+		 
+		 // Prepare Contraints for Btn4
+		 
+		 gcon.gridx = 1;
+		 
+		 // Add Control
+		 
+		 carte.add(btn4,gcon);
+		 
+		 gcon.gridx = 2;
+		 
+		 carte.add(btn5,gcon);
+		 
+		 gcon.gridx = 3;
+		 carte.add(btn6,gcon);
+		 
+		 gcon.gridx = 0;
+		 gcon.gridy = 2;
+		 gcon.gridwidth = 3;
+		 carte.add(btn7,gcon);
+		 
+		 gcon.gridy = 3;
+		 carte.add(btn8,gcon);
+		 
+		 gcon.gridx = 3;
+		 gcon.gridy = 2;
+		 gcon.gridwidth = 1;
+		 gcon.gridheight = 2;
+		 
+		 carte.add(btn9, gcon);
+		 
         return carte;
 
     }
@@ -162,24 +259,43 @@ public class Mapping_Window implements Navigate {
 
         JPanel bloc = global.getBloc();
         bloc.setLayout(new GridLayout(3,1));
+        
+        ImageIcon icon1 = new ImageIcon("src/main/resources/icon/sensor.png");
+        ImageIcon icon2 = new ImageIcon("src/main/resources/icon/ecran.jpg");
+        ImageIcon icon3 = new ImageIcon("src/main/resources/icon/prise.jpg");
 
-        JLabel ecran = new JLabel("Ecran");
-        ecran.setIcon(new ImageIcon(new ImageIcon(getUriOfFile("icon/ecran.jpg")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
-        ecran.setPreferredSize(new Dimension(100,30));
+        JLabel label1 = new JLabel(icon1, JLabel.CENTER);
+        JLabel label2 = new JLabel(icon2, JLabel.CENTER);
+        JLabel label3 = new JLabel(icon3, JLabel.CENTER);
 
-        JLabel prise = new JLabel("Prise");
-        prise.setPreferredSize(new Dimension(100,30));
-        prise.setIcon(new ImageIcon(new ImageIcon(getUriOfFile("icon/prise.jpg")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
+        DragMouseAdapter listener = new DragMouseAdapter();
+        label1.addMouseListener(listener);
+        label2.addMouseListener(listener);
+        label3.addMouseListener(listener);
 
-        JLabel capteur = new JLabel("Capteur");
-        capteur.setPreferredSize(new Dimension(100,30));
-        capteur.setIcon(new ImageIcon(new ImageIcon(getUriOfFile("icon/capteur.png")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT)));
+        
 
-        bloc.add(ecran);
-        bloc.add(capteur);
-        bloc.add(prise);
+        label1.setTransferHandler(new TransferHandler("icon"));
+        label2.setTransferHandler(new TransferHandler("icon"));
+        label3.setTransferHandler(new TransferHandler("icon"));
+    
+      
 
+        bloc.add(label1);
+        bloc.add(label2);
+        bloc.add(label3);
         bloc.repaint();
+    }
+    
+
+    private class DragMouseAdapter extends MouseAdapter {
+
+        public void mousePressed(MouseEvent e) {
+
+            JComponent c = (JComponent) e.getSource();
+            TransferHandler handler = c.getTransferHandler();
+            handler.exportAsDrag(c, e, TransferHandler.COPY);
+        }
     }
 
     private String getUriOfFile(String file) {
