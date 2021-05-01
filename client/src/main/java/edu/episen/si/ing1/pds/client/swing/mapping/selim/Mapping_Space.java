94708d5c99f7552bbde4dd0c63507310343f4b92
Selim <selim@MacBook-Pro-de-bouhassatine.local>
2021-05-01 21:10:12 +0200

    add a new class for beginning mapping workspace

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/swing/mapping/selim/Mapping_Space.java
@@ -0,0 +1,18 @@
+package edu.episen.si.ing1.pds.client.swing.mapping.selim;
+
+public class Mapping_Space {
+	
+	private String id_companies;
+	private String id_buildings;
+	private String id_floors;
+	private String id_workspace;
+
+	public Mapping_Space(String id_companies,String id_building, String id_floors, String id_workspace) {
+		this.id_companies = id_companies;
+		this.id_buildings = id_building;
+		this.id_floors = id_floors;
+		this.id_workspace = id_workspace;
+	}
+	
+	
+}
