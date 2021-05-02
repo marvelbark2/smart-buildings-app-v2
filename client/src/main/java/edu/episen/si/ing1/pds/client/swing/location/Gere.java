43f37e6540907924988a3dabe9e67ccc1babad2e
ludoweh <ludovicwehrung@gmail.com>
2021-05-02 23:43:46 +0200

    updating location for sql parts

--- client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Gere.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Gere.java
@@ -1,39 +1,70 @@
 package edu.episen.si.ing1.pds.client.swing.location;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import edu.episen.si.ing1.pds.client.network.Request;
+import edu.episen.si.ing1.pds.client.network.Response;
+import edu.episen.si.ing1.pds.client.utils.Utils;
 
 public class Gere implements Way {
 	 int nb_reserv= 16;
 	 JButton[] bouton = new JButton[nb_reserv];
 	 JLabel[] lab = new JLabel[nb_reserv];
 	
+	 
 	
 	
 	public void begin(LocationMenu men) {
 		JPanel pangere = men.getApp().getContext();
 		JPanel pg = new JPanel( );
 		
+		Request request = new Request();
+		request.setEvent("nb_reservation_list");
+		Map<String, Integer> hm = new HashMap<>();
+		//hm.put("company_id", ?);
+		request.setData(hm);
+		Response response = Utils.sendRequest(request);
+		Map<String, Object>  data = (Map<String, Object>) response.getMessage();
 		
 		
+		Request request2 = new Request();
+		request.setEvent("reservation_list");
+		Map<String, Integer> hm2 = new HashMap<>();
+		//hm.put("company_id", ?);--erreur
+		request.setData(hm);
+		//Response response = Utils.sendRequest(request);--erreur
+		//Map<String, Object> data = (Map<String, Object>) response.getMessage();--erreur
+		/*SocketConfig.Instance.setEnv(true);
+		Request request=new Request();
+		request.setEvent("random_offer");
+		// va chercher le nombre de reservation d'une entreprise
+		Response response = Utils.sendRequest(request);
+		int data_reserv = response.getMessage();*/
 		
-	      for (int r = 0; r < 16; r++) {  		
+		int data_reserv = 16;
+	      for (int r = 0; r < data_reserv; r++) {  		
 	    	  lab[r]=new JLabel("pos");// le texte change en fonction du bâtiment
-	    	  lab[r].setText("pos");
+	    	 // lab[r].setText("l'espace "+esp_reserv+"a été réservé");--erreur
 			  pg.add(lab[r]);
 	          bouton[r]= new JButton("annuler reservation"); 
 	          pg.add(bouton[r]);
 	          
+	          bouton[r].addActionListener(new ActionListener(){
+	        	  public void actionPerformed(ActionEvent e) {
+	        		 
+	        		// change l'etat de indisponible à dispible dans la table workspace
+	        		  
+	        	  }
+	          });
+	          
 	}
-	      pg.setLayout(new GridLayout(16,2));
-	     
-	      pg.invalidate();
-	      pg.validate();
-	      pg.repaint();
-	      pangere.add(pg);
-	
-
-}}
+	}
+}
--- client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Locat.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/swing/location/Locat.java
@@ -3,8 +3,16 @@ package edu.episen.si.ing1.pds.client.swing.location;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.*;
+
+import edu.episen.si.ing1.pds.client.network.Request;
+import edu.episen.si.ing1.pds.client.network.Response;
+import edu.episen.si.ing1.pds.client.network.SocketConfig;
+import edu.episen.si.ing1.pds.client.utils.Utils;
 
 
 public class Locat implements Way {
@@ -14,15 +22,16 @@ public class Locat implements Way {
     JTextField nbBureau = new JTextField(10);
     JTextField nbOpenspace = new JTextField(10);
     JTextField tailleMin = new JTextField(10);
-    JTextField prixMin = new JTextField(10);
+    JTextField persMax = new JTextField(10);
     JTextField prixMax = new JTextField(10);
     JCheckBox screen =new JCheckBox("ecran");
     JCheckBox captor =new JCheckBox("capteur");
     JCheckBox prise =new JCheckBox("prise");
     
+    int nbreserv = 0;
     int answer =5;
-    JLabel[] proposition = new JLabel[5];
-    JButton[] reserv = new JButton[5];
+    JLabel[] proposition = new JLabel[3];
+    JButton[] reserv = new JButton[3];
     
     
     JButton valid =new JButton("valider");
@@ -44,7 +53,7 @@ public class Locat implements Way {
 		    Tmax.setLabelFor(tailleMin);
 		 
 		    JLabel PersMax = new JLabel("nombre de personne", JLabel.LEFT);
-		    PersMax.setLabelFor(prixMin);
+		    PersMax.setLabelFor(persMax);
 		 
 		    JLabel Pmax = new JLabel("prix maximum", JLabel.LEFT);
 		    Pmax.setLabelFor(prixMax);
@@ -52,19 +61,126 @@ public class Locat implements Way {
 		   
 		    JButton retButton = new JButton("retour");
 		    p.setLayout(new GridLayout(12, 1));
+		    
+		    
 		
 		    
 		    valid.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
+					String Numbdesk = nbBureau.getText();
+					String NumbOp = nbOpenspace.getText();
+					int tmin = Integer.valueOf(tailleMin.getText()); 
+					int tmax = Integer.valueOf(tailleMax.getText());
+					int persmax = Integer.valueOf(persMax.getText());
+					int prixmax = Integer.valueOf(prixMax.getText());
 					
-						p.removeAll();
+					String listesp = "";
+					//int totnbdesk = 0;
+					//int totnbopensp = 0;
+					int totsize = 0;
+					int totpers = 0;
+					int totprice = 0;
+					
+					p.removeAll();
+					
+					for (int i = 0; i<3;i++) {
+						proposition[i]=new JLabel("");
 						
-						for (int i = 0; i<5;i++) {
-							proposition[i]=new JLabel("pos");// le texte change en fonction du bâtiment
-					    	  proposition[i].setText("cette offre contient x bureau fermé et y openspace pour une taille de z metre carré et un prix de x euros ");
+						
+				while(totsize<tmax && totpers<persmax &&totprice<prixmax ){
+					if(bureau_ferme.isSelected() && openspace.isSelected()) {
+						
+						Request request=new Request();
+						request.setEvent("random_offer");
+						Response response = Utils.sendRequest(request);
+						Map<String,Object> data=(Map<String, Object>) response.getMessage();
+						//totnbdesk = totnbdesk + 0;
+						//totnbopensp = totnbopensp +  0;
+					 	totsize = totsize + Integer.valueOf(data.get("size").toString());
+						totpers = totpers + Integer.valueOf(data.get("employee").toString());;
+						totprice = totprice + Integer.valueOf(data.get("price").toString());;
+						System.out.println(totsize);
+						listesp = listesp + data.get("id").toString();
+						
+					} else if(bureau_ferme.isSelected() && !openspace.isSelected()) {
+						Request request=new Request();
+						request.setEvent("random_offer_desk");
+						Response response = Utils.sendRequest(request);
+						Map<String,Object> data=(Map<String, Object>) response.getMessage();
+						//totnbdesk = totnbdesk + 0;
+						//totnbopensp = totnbopensp +  0;
+					 	totsize = totsize + Integer.valueOf(data.get("size").toString());
+						totpers = totpers + Integer.valueOf(data.get("employee").toString());;
+						totprice = totprice + Integer.valueOf(data.get("price").toString());;
+						
+						listesp = listesp + data.get("id").toString();
+					} else {
+						Request request=new Request();
+						request.setEvent("random_offer_openspace");
+						Response response = Utils.sendRequest(request);
+						Map<String,Object> data=(Map<String, Object>) response.getMessage();
+					
+					
+					//totnbdesk = totnbdesk + 0;
+					//totnbopensp = totnbopensp +  0;
+				 	totsize = totsize + Integer.valueOf(data.get("size").toString());
+					totpers = totpers + Integer.valueOf(data.get("employee").toString());;
+					totprice = totprice + Integer.valueOf(data.get("price").toString());;
+					
+					listesp = listesp +" , "+ data.get("id").toString();
+					}
+				}
+					
+					
+					// va chercher les données d'un espace et les mets dans un tableau dans l'ordre id,size,maxpers,price,type
+					//si je deviens chaud je récupère le nom de la salle, so étage et son bâtiment pour en faire une liste
+					
+					
+					
+					
+					
+					
+				
+					
+						// le texte change en fonction du bâtiment
+					    	  proposition[i].setText("cette offre contient les salles: "+ listesp+ " dont  x bureau fermé et y"
+					    	  		+ " openspace pour une taille de "+
+							totsize+" metres carrés et un prix de "+totprice +" euros, il peut contenir "+totpers+" employés ");
 					    	  p.add(proposition[i]);
 					    	  reserv[i]= new JButton("réserver"); 
 					          p.add(reserv[i]);
+			
+								listesp = "";
+								// totnbdesk = 0;
+								// totnbopensp = 0;
+								totsize = 0;
+								totpers = 0;
+								totprice = 0;
+    
+					          reserv[i].addActionListener(new ActionListener(){
+					        	  public void actionPerformed(ActionEvent e) {
+					        		  nbreserv++;
+					        		  
+					        		  Request request = new Request();
+					        			request.setEvent("done_reservation");
+					        			Map<String, Object> hm = new HashMap<>();
+					        			//hm.put("list_workspace", listesp);--erreur
+					        			hm.put("reserv_numb",nbreserv);
+					        			//hm.put("id_compa",id_compa);--erreur
+					        			//hm.put("id_worksp",listesp);--erreur
+					        			request.setData(hm);
+					        			
+
+					        			Response response = Utils.sendRequest(request);
+
+					        			Map<String, Object>  data = (Map<String, Object>) response.getMessage();
+					        		  //reserv[i].setText("réservation acceptée !");--erreur
+					        		  
+					 
+					        		// change l'etat de disponible à indispible dans la table workspace
+					        		  
+					        	  }
+					          });
 						}
 						
 					     p.add(retButton) ;
@@ -78,13 +194,15 @@ public class Locat implements Way {
 				}
 			});
 		    
+		    
+		    
 		    retButton.addActionListener(new ActionListener() {
 				
 				public void actionPerformed(ActionEvent e) {
 					p.removeAll();
 					
-				    p.add(bureau_ferme);
-				    p.add(openspace);
+					p.add(bureau_ferme);
+				   	p.add(openspace);
 				    p.add(Nbdesk);
 				    p.add(nbBureau);
 				    p.add(Nbopenspace);
@@ -94,7 +212,7 @@ public class Locat implements Way {
 				    p.add(Tmax);
 				    p.add(tailleMax);
 				    p.add(PersMax);
-				    p.add(prixMin);
+				    p.add(persMax);
 				    p.add(Pmax);
 				    p.add(prixMax);  
 				    p.add(screen);
@@ -112,7 +230,7 @@ public class Locat implements Way {
 		           
 		        
 		    p.add(bureau_ferme);
-		    p.add(openspace);
+		   	p.add(openspace);
 		    p.add(Nbdesk);
 		    p.add(nbBureau);
 		    p.add(Nbopenspace);
@@ -122,7 +240,7 @@ public class Locat implements Way {
 		    p.add(Tmax);
 		    p.add(tailleMax);
 		    p.add(PersMax);
-		    p.add(prixMin);
+		    p.add(persMax);
 		    p.add(Pmax);
 		    p.add(prixMax); 
 		    p.add(screen);
